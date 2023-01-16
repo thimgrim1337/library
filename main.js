e83dd19cbaf32fae13b2122e007ddd47145bfa5f
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2023-01-16 17:58:01 +0100

    Create openModal, closeModal and clearModal methods to class UI

--- main.js
+++ main.js
@@ -2,42 +2,8 @@
 
 const plusBtn = document.querySelector('.btn-plus');
 const addBtn = document.querySelector('.btn-add');
-const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.mdi-close-circle-outline');
 const books = document.querySelector('.books');
-
-/* let myLibrary = [
-  {
-    title: 'Song of Ice and Fire',
-    author: 'George R. R. Martin',
-    pages: 550,
-    read: false,
-  },
-  {
-    title: 'Harry Potter',
-    author: 'J.K. Rowling',
-    pages: 450,
-    read: false,
-  },
-  {
-    title: 'Foundation',
-    author: 'R. Asimov',
-    pages: 500,
-    read: false,
-  },
-  {
-    title: 'The Lord of The Ring',
-    author: 'J.R.R. Tolkien',
-    pages: 600,
-    read: false,
-  },
-  {
-    title: 'Song of Ice and Fire',
-    author: 'George R. R. Martin',
-    pages: 550,
-    read: false,
-  },
-]; */
 
 class Book {
   constructor(
@@ -69,20 +35,7 @@ class Library {
     UI.createBookCard(this.library, book);
   };
 }
-let myLibrary = new Library();
-
-/* const addBookToLibrary = (e) => {
-  e.preventDefault();
-  const title = document.querySelector('#bookTitle').value;
-  const author = document.querySelector('#bookAuthor').value;
-  const pages = document.querySelector('#bookPages').value;
-  const read = document.querySelector('#bookRead').checked;
-
-  const book = new Book(title, author, pages, read);
-
-  myLibrary.push(book);
-  createBookCard(book);
-}; */
+const myLibrary = new Library();
 
 class UI {
   static createBookCard = (library, book) => {
@@ -102,56 +55,34 @@ class UI {
     div.className = 'book';
     div.dataset.index = index;
 
-    books.appendChild(div);
-    closeModal();
+    books.insertBefore(div, books.firstChild);
+    UI.closeModal();
+  };
+
+  static openModal = () => {
+    this.modal.style.display = 'block';
+  };
+
+  static closeModal = () => {
+    this.modal.style.display = 'none';
+    UI.clearModal();
+  };
+
+  static clearModal = () => {
+    document.querySelector('#bookTitle').value = '';
+    document.querySelector('#bookAuthor').value = '';
+    document.querySelector('#bookPages').value = '';
+    document.querySelector('#bookRead').checked = false;
   };
 }
+UI.modal = document.querySelector('.modal');
 
-const ui = new UI();
-
-/* const createBookCard = (book) => {
-  const books = document.querySelector('.books');
-  const div = document.createElement('div');
-  const index = myLibrary.indexOf(book);
-  div.innerHTML += `
-  <h2>${book.title}</h2>
-  <h3>${book.author}</h3>
-  <span>${book.pages} pages</span>
-  <button data-index=${index} class="btn-read ${
-    book.read === true ? 'green' : ''
-  }">Read ?</button>
-  <button data-index=${index}
-  class="btn-remove">Remove</button>
-  `;
-  div.className = 'book';
-  div.dataset.index = index;
-
-  books.appendChild(div);
-  closeModal();
-}; */
-
-const showBooks = () => {
+/* const showBooks = () => {
   if (myLibrary.length === 0) return;
   for (const book of myLibrary) {
     createBookCard(book);
   }
-};
-
-const openModal = () => {
-  modal.style.display = 'block';
-};
-
-const closeModal = () => {
-  modal.style.display = 'none';
-  clearModal();
-};
-
-const clearModal = () => {
-  document.querySelector('#bookTitle').value = '';
-  document.querySelector('#bookAuthor').value = '';
-  document.querySelector('#bookPages').value = '';
-  document.querySelector('#bookRead').checked = false;
-};
+}; */
 
 const setRead = (e) => {
   e.target.classList.toggle('green');
@@ -179,16 +110,14 @@ const updateDataIndex = () => {
   console.log(indexes);
 };
 
-// showBooks();
-
 const readBtn = document.getElementsByClassName('btn-read');
 [...readBtn].forEach((btn) => btn.addEventListener('click', setRead));
 
 const removeBtn = document.getElementsByClassName('btn-remove');
 [...removeBtn].forEach((btn) => btn.addEventListener('click', removeBook));
 
-plusBtn.addEventListener('click', openModal);
+plusBtn.addEventListener('click', UI.openModal);
 
 addBtn.addEventListener('click', myLibrary.addBook);
 
-modalClose.addEventListener('click', closeModal);
+modalClose.addEventListener('click', UI.closeModal);
